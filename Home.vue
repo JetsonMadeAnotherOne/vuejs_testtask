bcf805a4012c6fd76243dac82372900951ef19ef
MaximUstianov <50552333+MaximUstianov@users.noreply.github.com>
2019-09-15 18:52:08 +0300

    Add files via upload

---
+++ Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+        <p>helo helooooooo</p>
+
+        <a href="/todos">Todos</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
